7110f6aca26992ddcf43b8f01671e9395d6c9d6b
konrad101296 <161070317+konrad101296@users.noreply.github.com>
2024-03-19 20:29:32 +0100

    Update Program.cs

--- Week2Lesson8ExerciseNr10/Program.cs
+++ Week2Lesson8ExerciseNr10/Program.cs
@@ -1,24 +1,19 @@
-﻿Console.WriteLine("enter first number");
+Console.WriteLine("enter first number");
 int number1 = int.Parse(Console.ReadLine());
 Console.WriteLine("enter second number");
 int number2 = int.Parse(Console.ReadLine());
-int bigger = 0;
+int biggerNumber = 0;
 if (number1 >= number2)
 {
-    bigger = number1;
+    biggerNumber = number1;
 }
 else if (number1 <= number2)
 {
-    bigger = number2;
+    biggerNumber = number2;
 }
-for (int i = bigger; i >= bigger; i++)
+for (int i = biggerNumber; i >= biggerNumber; i++)
 {
-    if (number1 > number2 && i % number1 == 0 && i % number2 == 0)
-    {
-        Console.WriteLine($"The least common multiple is:{i}");
-        break;
-    }
-    else if (number1 < number2 && i % number2 == 0 && i % number1 == 0)
+    if ((number1 > number2 || number1 < number2 )&& i % number1 == 0 && i % number2 == 0)
     {
         Console.WriteLine($"The least common multiple is:{i}");
         break;
